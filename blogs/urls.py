0f466f5e321883339e7b462803b42b3d5b0af6ec
ahsharifi <ahsharifidev@gmail.com>
2026-09-06 01:07:39 +0330

    add blogs to admin

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('blog-details/<int:id>', views.details)
+    path('', views.index, name='blogs_list'),
+    path('blog-details/<int:id>', views.details, name='blogs_details')
 ]
