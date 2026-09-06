761d405421dc84dc45e9197ecda533732be15974
ahsharifi <ahsharifidev@gmail.com>
2026-09-07 00:11:13 +0330

    add product views

--- shop/views.py
+++ shop/views.py
@@ -1,36 +1,13 @@
 from django.shortcuts import render
-
-data = [
-    {
-      "id": 1,
-      "name": "Item1",
-      "category": "category1",
-      "price": 10000
-    },
-    {
-      "id": 2,
-      "name": "Item2",
-      "category": "category1",
-      "price": 20000
-    },
-    {
-      "id": 3,
-      "name": "Item3",
-      "category": "category2",
-      "price": 33000
-    }
-  ]
+from shop.models import Product
 
 def index(request):
+  data = Product.objects.all()
+
   return render(request, 'shop/index.html', { 'products': data })
 
 def details(request, id):
 
-  selected = {}
-
-  for item in data:
-    if item["id"] == id:
-      selected = item
-      break
+  selected = Product.objects.get(id = id)
 
   return render(request, 'shop/details.html', { "product": selected })
