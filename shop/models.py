9fa28a70be2b47aa13a67fd83724492fc65ef881
ahsharifi <ahsharifidev@gmail.com>
2026-09-07 00:08:03 +0330

    add product models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  title = models.CharField(max_length=200)
+  price = models.IntegerField()
+  status = models.BooleanField()
+
+  def __str__(self):
+    return self.title
