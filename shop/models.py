277b1c13242e940c276d5e6fd0b8f7b8a76b994b
ahsharifi <ahsharifidev@gmail.com>
2026-09-09 23:42:04 +0330

    add forigin keys

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Category(models.Model):
+  name = models.CharField()
+  description = models.TextField()
+
 class Product(models.Model):
   title = models.CharField(max_length=200)
+  description = models.TextField(null=True)
   price = models.IntegerField()
+  category_id = models.ForeignKey(Category, models.CASCADE, null=True)
   status = models.BooleanField()
 
   def __str__(self):
