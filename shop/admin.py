9780817bf3a164b2b91cabd833f9ef8f497c15ad
ahsharifi <ahsharifidev@gmail.com>
2026-09-11 22:06:27 +0330

    add category relationship

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from shop.models import Product
+from shop.models import Product, Category
 
 admin.site.register(Product)
+admin.site.register(Category)
--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Category(models.Model):
-  name = models.CharField()
+  title = models.CharField()
   description = models.TextField()
+
+  def __str__(self):
+    return self.title
 
 class Product(models.Model):
   title = models.CharField(max_length=200)
