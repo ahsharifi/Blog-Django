e26aa4948418cef46a71245cc29ff8d65a60da4f
ahsharifi <ahsharifidev@gmail.com>
2026-09-15 12:05:36 +0330

    [Add]: new db types

--- shop/models.py
+++ shop/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 class Category(models.Model):
-  title = models.CharField()
-  description = models.TextField()
+    title = models.CharField(
+        max_length=100,
+        verbose_name="نام دسته بندی",
+    )
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
-  title = models.CharField(max_length=200)
-  description = models.TextField(null=True)
-  price = models.IntegerField()
-  category_id = models.ForeignKey(Category, models.CASCADE, null=True)
-  status = models.BooleanField()
+    title = models.CharField(max_length=200, verbose_name="نام محصول")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="لینک")
+    description = models.TextField(null=True, blank=True, verbose_name="توضیحات")
+    price = models.DecimalField(max_digits=12, decimal_places=0, verbose_name="قیمت")
+    category_id = models.ForeignKey(Category, models.CASCADE, null=True, verbose_name="دسته بندی")
+    status = models.BooleanField(verbose_name="وضعیت", default=True)
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
